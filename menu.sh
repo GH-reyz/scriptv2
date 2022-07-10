3b5f914f3f7852617507f728a8af94e8d6521361
GH-reyz <106010948+GH-reyz@users.noreply.github.com>
2022-07-11 02:19:33 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 clear
 red='\e[1;31m'
-bred='\[41m'
+white='\e[1;37m'
+bred='\e[41m'
 green='\e[0;32m'
 blue='\e[0;34m'
 blue_b='\e[0;94m'
@@ -23,6 +24,19 @@ DOMAIN=$(cat /etc/v2ray/domain)
 cekxray="$(openssl x509 -dates -noout < /etc/v2ray/v2ray.crt)"                                      
 expxray=$(echo "${cekxray}" | grep 'notAfter=' | cut -f2 -d=)
 name=$(curl -sS https://raw.githubusercontent.com/GH-reyz/GH-reyz/main/Register%20IP | grep $IPVPS | awk '{print $2}')
+city=$(curl -s https://ipinfo.io/json | grep -o 'city": "[^"]*' | grep -o '[^"]*$')
+TIME=$(curl -sS ip-api.com | grep -w "timezone" | awk '{print $3}' | cut -d'"' -f2 | tee -a /etc/afak.conf)
+ISP=$(curl -sS ip-api.com | grep -w "isp" | awk '{print $3,$4,$5,$6,$7,$8,$9}' | cut -d'"' -f2 | cut -d',' -f1 | tee -a /etc/afak.conf)
+up=$(uptime | awk '{ $1=$2=$(NF-6)=$(NF-5)=$(NF-4)=$(NF-3)=$(NF-2)=$(NF-1)=$NF=""; print }')
+cores=$(awk -F: '/model name/ {core++} END {print core}' /proc/cpuinfo)
+freq=$(awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo)
+tram=$(free -m | awk 'NR==2 {print $2}')
+cpu_usage1="$(ps aux | awk 'BEGIN {sum=0} {sum+=$3}; END {print sum}')"
+cpu_usage="$((${cpu_usage1/\.*/} / ${corediilik:-1}))"
+uram=$(free -m | awk 'NR==2 {print $3}')
+fram=$(free -m | awk 'NR==2 {print $4}')
+cpu_usage+=" %"
+cname=$(awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo)
 exp=$(curl -sS https://raw.githubusercontent.com/GH-reyz/GH-reyz/main/Register%20IP | grep $IPVPS | awk '{print $3}')
 
 
@@ -37,10 +51,20 @@ echo -e  " ${red} Premium Script"
 echo -e  " ${red}═════════════════════════════════════════════════════════════════ " 
 echo -e  " ${bred}                      • SERVER INFORMATION •              "${NC}
 echo -e  " ${red}═════════════════════════════════════════════════════════════════ " 
+echo -e  " ${red}CPU MODEL                   : $cname"
+echo -e  " ${red}NUMBER OF CORES             : $cores"
+echo -e  " ${red}CPU USAGE                   : $cpu_usage1
+echo -e  " ${red}TOTAL RAM                   : $tram MB"
+echo -e  " ${red}USED RAM                    : $uram MB"
+echo -e  " ${red}FREE RAM                    : $fram MB"
+echo -e  " ${red}SYSTEM UPTIME               : $up"
+echo -e  " ${red}ISP/PROVIDER NAME           : $ISP"
+echo -e  " ${red}CITY LOCATION               : $city${NC}"
+echo -e  " ${red}TIME LOCATION               : $TIME"
 echo -e  " ${red}VPS TYPE                    : PREMIUM"
 echo -e  " ${red}IP VPS NUMBER               : $IPVPS${NC}"
 echo -e  " ${red}DOMAIN                      : $DOMAIN${NC}"
-echo -e  " ${red}SCRIPT VERSION              : REYZ-V4 (V1)"
+echo -e  " ${red}SCRIPT VERSION              : REYZ-V4 (V2)"
 echo -e  " ${red}OS VERSION                  : `hostnamectl | grep "Operating System" | cut -d ' ' -f5-`"${NC}
 echo -e  " ${red}KERNEL VERSION              : `uname -r`${NC}"
 echo -e  " ${red}EXP DATE CERT V2RAY/XRAY    : $expxray${NC}"
@@ -74,8 +98,8 @@ echo -e  " [ 18 ] CHECK SERVICE ERROR"
 echo -e  " [ 19 ] UPDATE SCRIPT"
 echo -e  " [  0 ] EXIT MENU${NC} "
 echo -e  " ${red}═════════════════════════════════════════════════════════════════ " | lolcat
-echo -e  "  Premium Script By Reyz-V4" | lolcat
-echo -e  "  Thank You For Using Script By Reyz-V4" | lolcat
+echo -e  " ${white}Premium Script By Reyz-V4" 
+echo -e  " ${white}Thank You For Using Script By Reyz-V4" 
 echo -e  " ${red}═════════════════════════════════════════════════════════════════ " | lolcat
 echo -e  "  "
 echo -e "\e[1;31m"
