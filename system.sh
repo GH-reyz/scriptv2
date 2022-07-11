9218669f7de7d2cadd728a2c265c679477d895e0
GH-reyz <106010948+GH-reyz@users.noreply.github.com>
2022-07-11 23:37:44 +0800

    Update system.sh

--- system.sh
+++ system.sh
@@ -3,19 +3,18 @@ clear
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
+ok='\e[0;47;30m'
 
 echo -e  " ${red}=========================================================================="
-echo -e  "                                                SYSTEM MENU                                                                                                  "
+echo -e  " ${ok}                                      SYSTEM MENU"                                                                                                  "
 echo -e  " ${red}=========================================================================="
-echo -e  " [  1 ] ADD/CHANGE DOMAIN VPS"
-echo -e  " [  2 ] CHANGE DNS SERVER"
-echo -e  " [  3 ] RENEW V2RAY AND XRAY CERTIFICATION"
-echo -e  " [  4 ] WEBMIN MENU"
-echo -e  " [  5 ] CHECK RAM USAGE"
-echo -e  " [  6 ] SPEEDTEST VPS"
-echo -e  " [  7 ] DISPLAY SYSTEM INFORMATION"
-echo -e  " [  8 ] CHECK STREAM GEO LOCATION"
-echo -e  " [  9 ] BANDWIDTH USAGE"
+echo -e  " ${red}[  1 ] ADD/CHANGE DOMAIN VPS           [  2 ] CHANGE PORT SERVICE"
+echo -e  " [ 3 ] CHANGE DNS SERVER               [ 4 ] RENEW CERTIFICATION"
+echo -e  " [ 5 ] WEBMIN MENU                     [ 6 ] CHECK RAM USAGE"
+echo -e  " [ 7 ] REBOOT VPS                      [ 8 ] SPEEDTEST VPS"
+echo -e  " [ 9 ] SYSTEM INFORMATION              [ 10 ] CHECK GEO LOCATION"
+echo -e  " [ 11] CHECK SERVICE ERROR             [ 12 ] UPDATE SCRIPT"
+echo -e  " [ 13] UPDATE SCRIPT                   [  0 ] EXIT MENU${NC} "
 echo -e "=========================================================================="
 echo -e "${red}[ 0 ] EXIT TO MENU"
 echo -e "=========================================================================="
@@ -25,40 +24,49 @@ echo -e "\e[1;31m"
 read -p  "     Please select an option :  " v2raycore
 echo -e "\e[0m"
 case $v2raycore in
-    1)
-    add-host
-    ;;
-    2)
-    mdns
-    ;;
-    3)
-    recert-xrayv2ray
-    ;;
-    4)
-    wbmn
-    ;;
-    5)
-    ram
-    ;;
-    6)
-    speedtest
-    ;;
-    7)
-    info
-    ;;
-    8)
-    nf
-    ;;
-    9)
-    vnstat
-    ;;
-    x)
-    exit
-    ;;
-	0)
-    sleep 0.5
-    clear
-    menu
+   1)
+   add-host
+   ;;
+   2)
+   change
+   ;;
+   3)
+   mdns
+   ;;
+   4)
+   recert-xrayv2ray
+   ;;
+   5)
+   wbmn
+   ;;
+   6)
+   ram
+   ;;
+   7)
+   reboot
+   ;;
+   8)
+   speedtest
+   ;;
+   9)
+   info
+   ;;
+   10)
+   nf
+   ;;
+   11)
+   checksystem  
+   ;;
+  12)
+   vnstat
+   ;; 
+  13)
+   update  
+   ;;     
+   0)
+   sleep 0.5
+   clear
+   jinggo 
     ;;
     *)
    echo -e "ERROR!! Please Enter an Correct Number"
