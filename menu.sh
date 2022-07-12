d38c88867aca79a112049095e70cc1a7d5b8a6fa
GH-reyz <106010948+GH-reyz@users.noreply.github.com>
2022-07-12 13:27:54 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -38,7 +38,46 @@ fram=$(free -m | awk 'NR==2 {print $4}')
 cpu_usage+=" %"
 cname=$(awk -F: '/model name/ {name=$2} END {print name}' /proc/cpuinfo)
 exp=$(curl -sS https://raw.githubusercontent.com/GH-reyz/GH-reyzv2/main/registerv2 | grep $IPVPS | awk '{print $3}')
-
+bulan=$(date +%b)
+vnstat_profile=$(vnstat | sed -n '3p' | awk '{print $1}')
+vnstat -i ${vnstat_profile} >/root/t1
+today=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $8}')
+today_v=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $9}')
+today_rx=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $2}')
+today_rxv=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $3}')
+today_tx=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $5}')
+today_txv=$(vnstat -i ${vnstat_profile} | grep today | awk '{print $6}')
+if [ "$(grep -wc ${bulan} /root/t1)" != '0' ]; then
+  bulan=$(date +%b)
+  month=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $9}')
+  month_v=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $10}')
+  month_rx=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $3}')
+  month_rxv=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $4}')
+  month_tx=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $6}')
+  month_txv=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $7}')
+else
+  bulan=$(date +%Y-%m)
+  month=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $8}')
+  month_v=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $9}')
+  month_rx=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $2}')
+  month_rxv=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $3}')
+  month_tx=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $5}')
+  month_txv=$(vnstat -i ${vnstat_profile} | grep "$bulan " | awk '{print $6}')
+fi
+if [ "$(grep -wc yesterday /root/t1)" != '0' ]; then
+  yesterday=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $8}')
+  yesterday_v=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $9}')
+  yesterday_rx=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $2}')
+  yesterday_rxv=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $3}')
+  yesterday_tx=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $5}')
+  yesterday_txv=$(vnstat -i ${vnstat_profile} | grep yesterday | awk '{print $6}')
+else
+  yesterday=NULL
+  yesterday_v=NULL
+  yesterday_rx=NULL
+  yesterday_rxv=NULL
+  yesterday_tx=NULL
+  yesterday_txv=NULL
 
 echo -e  " ${red}   ____                      _    ____ __ " 
 echo -e  "   / __ \___  __  ______     | |  / / // / " 
@@ -70,6 +109,11 @@ echo -e  " ${red}EXP DATE CERT V2RAY/XRAY    : $expxray${NC}"
 echo -e  " ${red}CLIENT NAME                 : $name${NC}"
 echo -e  " ${red}EXP SCRIPT ACCSESS          : $exp${NC}"
 echo -e  " ${red}CONTACT TELEGRAM            : @GHReyz" 
+echo -e " ${yellow}═════════════════════════════════════════════════════════════════"
+echo -e "  ${bred}TRAFFIC           TODAY          YESTERDAY          MONTH" 
+echo -e "  ${white}UPLOAD            $today_tx $today_txv      $yesterday_tx $yesterday_txv         $month_tx $month_txv"
+echo -e "  DOWNLOAD          $today_rx $today_rxv      $yesterday_rx $yesterday_rxv         $month_rx $month_rxv"
+echo -e "  TOTAL             $today $today_v      $yesterday $yesterday_v         $month $month_v"
 echo -e  " ${red}═════════════════════════════════════════════════════════════════ " 
 echo -e  " ${bred}                         • MAIN MENU •                           "${NC}                                      
 echo -e  " ${red}═════════════════════════════════════════════════════════════════ " 
